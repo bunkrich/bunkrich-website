d9d641412e606b9dbfc01f61f79a4d49c1a0caa3
Brent Unkrich <brentunkrich@brents-air.lan>
2019-02-06 21:59:57 -0500

    added calc

---
+++ js/main.js
@@ -0,0 +1,72 @@
+let barNet = 0;
+let serverCash = 0;
+let totalTips = 0;
+let cashTips = 0;
+
+let barPercent = .15;
+let barTipOutCash = 0;
+let barTipOutCheck = 0;
+
+let busserPercent = .15;
+let busserOTR = false;
+let busserTipOutCash = 0;
+let busserTipOutCheck = 0;
+
+let takeHomeTipsCash = 0;
+let takeHomeTipsCheck = 0;
+
+let tipTextBusser = "";
+let text = "";
+let text2="";
+let text3="";
+
+function tipMath() {
+  let totalTipsInput = document.getElementById("totalTips");
+  let barNet = Number(document.getElementById("barNet").value);
+  let serverCash = Number(document.getElementById("serverCash").value);
+  let totalTips = 0;
+
+  if (isNaN(barNet) || barNet < 0) {
+    barNet = barNet * -1;
+    totalTips = barNet + serverCash;
+    cashTips = serverCash;
+    text = "You get $" + barNet + " from the bar. This is what you input into the spreadsheet.";
+    text2 = "Your total tips are $" + totalTips + ".";
+    text3 = "Your total cash tips to tip out on are: $" + cashTips;
+
+  } else {
+    totalTips = (barNet - serverCash) * -1;
+    cashTips = serverCash - barNet;
+    text = "You owe the bar $" + barNet + ".";
+    text2 = "Your total tips are $" + totalTips + "."
+    text3 = "Your total cash tips to tip out on are $" + cashTips;
+
+  }
+
+
+  totalTipsInput.setAttribute("value", totalTips);
+
+  let busserOTR = document.getElementById("busserOTR").checked;
+  if (busserOTR === true) {
+    console.log("true! Busser will get a check")
+    busserTipOutCash = Math.ceil(cashTips * busserPercent);
+
+  } else {
+    console.log("false! You gotta tip the busser out!")
+    busserTipOutCash = Math.ceil(totalTips * busserPercent);
+  }
+
+  tipTextBusser = "$"+busserTipOutCash;
+  barTipOutCash = Math.ceil(cashTips * barPercent);
+  tipTextBar = "$"+barTipOutCash;
+  serverTakeHomeCash = cashTips - barTipOutCash - busserTipOutCash;
+  tipTextServer = "$"+serverTakeHomeCash;
+
+  document.getElementById("tip-out-box").style.display = "block";
+  document.getElementById("tip-out").innerHTML = text
+  document.getElementById("tip-out-2").innerHTML = text2;
+  document.getElementById("tip-out-3").innerHTML = text3;
+  document.getElementById("cash-out-bar").innerHTML = tipTextBar;
+  document.getElementById("cash-out-busser").innerHTML = tipTextBusser;
+  document.getElementById("cash-out-server").innerHTML = tipTextServer;
+}
